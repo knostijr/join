c500e477b8b83327ba717904e95a34d700b75dca
knostijr <christophkonst@icloud.com>
2026-06-30 11:46:11 +0200

    fix: add environment.ts for Vercel deployment

---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrhXwPFb3mRB6y6KRPi6nEy9qVZbZWUFY",
+  authDomain: "joindb-ff8f3.firebaseapp.com",
+  projectId: "joindb-ff8f3",
+  storageBucket: "joindb-ff8f3.firebasestorage.app",
+  messagingSenderId: "401228088537",
+  appId: "1:401228088537:web:d17ae510db4bde4797aac8",
+  measurementId: "G-HCD274GY9Y"
+};
+
+export const environment = {
+  production: false,
+};
